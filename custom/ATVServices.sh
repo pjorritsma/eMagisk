8057933680e66bd7f0ff4ed636feb963b617648e
Astu04 <Astu04@users.noreply.github.com>
2023-07-31 02:08:16 +0200

    it's always dns

--- custom/ATVServices.sh
+++ custom/ATVServices.sh
@@ -4,6 +4,7 @@
 POGOPKG=com.nianticlabs.pokemongo
 UNINSTALLPKGS="com.ionitech.airscreen cm.aptoidetv.pt com.netflix.mediaclient org.xbmc.kodi com.google.android.youtube.tv"
 CONFIGFILE='/data/local/tmp/emagisk.config'
+setprop net.dns1 1.1.1.1 && setprop net.dns2 4.4.4.4
 
 # Check for the mitm pkg
 
